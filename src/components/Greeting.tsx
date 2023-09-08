34828a2c7d27bc331b7158e038a3eff2533e002c
Spectrum-Dwight <dwight@spectrumware.net>
2023-09-08 14:57:47 -0500

    fix: naming

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,10 +1,9 @@
-type Props = {
+type GreetingProps = {
   name: string;
   age: number;
 };
 
-export default function Greeting({name, age}: Props) {
-
+export default function Greeting({ name, age }: GreetingProps) {
   return (
     <p>
       Hello, my name is {name} and I am {age} years old.
